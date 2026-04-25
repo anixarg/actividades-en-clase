794a694174bbf949cdad160e1c8a61b73a5b15ad
anixarg <anabellajk200@gmail.com>
2026-04-25 18:53:12 -0300

    Add files via upload

---
+++ EJERCICIO 3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    // Funcion que calcula el area del circulo y devuelve el resultado
+    public static double CalcularAreaCirculo(double radio)
+    {
+        return Math.PI * Math.Pow(radio, 2);
+    }
+
+    public static void ImprimirResultado(string texto, double resultado)
+    {
+        Console.WriteLine($"--- {texto} {resultado:F2} ---");
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese el radio del círculo: ");
+        double radio = Convert.ToDouble(Console.ReadLine());
+
+        double area = CalcularAreaCirculo(radio);
+        ImprimirResultado("EL RESULTADO ES:", area);
+    }
+}
