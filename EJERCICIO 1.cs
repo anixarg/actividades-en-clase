ba81ed1d260c54a2c824089f8d9d2d5550238031
anixarg <anabellajk200@gmail.com>
2026-04-25 16:37:40 -0300

    Add files via upload

---
+++ EJERCICIO 1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // EL USUARIO INGRESA SU EDAD
+        Console.Write("Ingrese la edad: ");
+        int edad;
+        bool esNumero = int.TryParse(Console.ReadLine(), out edad);
+
+        // VALIDACION DEL NUMERO
+        if (!esNumero)
+        {
+            Console.WriteLine("Por favor, ingrese un número válido para la edad.");
+        }
+        else
+        {
+            bool esMayor = EsMayorDeEdad(edad);
+            if (esMayor)
+            {
+                Console.WriteLine("Es mayor de edad.");
+            }
+            else
+            {
+                Console.WriteLine("No es mayor de edad.");
+            }
+        }
+    }
+
+    // VERIFICACION DE QUE SI ES MAYOR
+    public static bool EsMayorDeEdad(int edad)
+    {
+        return edad >= 18;
+    }
+}
