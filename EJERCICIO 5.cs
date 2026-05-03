9361d2f2d31a82dceee48f802fdf040303e19d04
anixarg <anabellajk200@gmail.com>
2026-05-03 20:38:37 -0300

    Add files via upload

---
+++ EJERCICIO 5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    // funcionamiento para retirar
+    public static double RealizarRetiro(double saldo, double retiro)
+    {
+        if (retiro > saldo)
+        {
+            Console.WriteLine("Error: El monto a retirar excede el saldo actual.");
+            return saldo;
+        }
+        else
+        {
+            return saldo - retiro;
+        }
+    }
+
+    static void Main()
+    {
+        double saldoActual = 1000; // saldo inicial
+        Console.WriteLine("Saldo inicial: " + saldoActual);
+
+        Console.Write("Ingrese el monto a retirar: ");
+        double montoARetirar = Convert.ToDouble(Console.ReadLine());
+
+        saldoActual = RealizarRetiro(saldoActual, montoARetirar);
+        Console.WriteLine("Saldo restante: " + saldoActual);
+    }
+}
