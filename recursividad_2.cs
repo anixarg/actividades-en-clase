6fb4171610d7823f857227b7bf474527f5de667e
anixarg <anabellajk200@gmail.com>
2026-05-23 20:48:14 -0300

    Add files via upload

---
+++ recursividad_2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static int CalcularPotencia(int bas, int exponente)
+    {
+        if (exponente == 0)
+        {
+            return 1;
+        }
+
+        return bas * CalcularPotencia(bas, exponente - 1);
+    }
+
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Ingrese la base: ");
+            int bas = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el exponente: ");
+            int exponente = int.Parse(Console.ReadLine());
+
+            if (exponente < 0)
+            {
+                throw new Exception("Esta funcion no acepta exponentes negativos");
+            }
+
+            int resultado = CalcularPotencia(bas, exponente);
+
+            Console.WriteLine("Resultado: " + resultado);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Debe ingresar numeros enteros.");
+        }
+    }
+}
