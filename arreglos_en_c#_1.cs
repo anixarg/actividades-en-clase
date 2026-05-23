bb86e567f66dd3538f31342cb01104a1b4517375
anixarg <anabellajk200@gmail.com>
2026-05-23 20:44:29 -0300

    Add files via upload

---
+++ arreglos_en_c#_1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    // Función recursiva para contar dígitos
+    static int ContarDigitos(int n)
+    {
+        // Caso base
+        if (n < 10)
+        {
+            return 1;
+        }
+
+        // Caso recursivo
+        return 1 + ContarDigitos(n / 10);
+    }
+
+    static void Main()
+    {
+        int numero = 12543;
+
+        int cantidad = ContarDigitos(numero);
+
+        Console.WriteLine("El numero tiene " + cantidad + " digitos.");
+    }
+}
+
