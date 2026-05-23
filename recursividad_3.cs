874c114ed90ad0011b1e4efc898eedabcffc47e1
anixarg <anabellajk200@gmail.com>
2026-05-23 20:53:15 -0300

    Add files via upload

---
+++ recursividad_3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void InvertirNumero(int n)
+    {
+        if (n < 10)
+        {
+            Console.Write(n);
+            return;
+        }
+
+        Console.Write(n % 10);
+
+        InvertirNumero(n / 10);
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese un numero: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        Console.Write("Numero invertido: ");
+
+        InvertirNumero(numero);
+    }
+}
+
