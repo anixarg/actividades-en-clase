aab7f6dba8f103a008f7c7df9f99063e4a963a1a
anixarg <anabellajk200@gmail.com>
2026-05-24 20:47:02 -0300

    Add files via upload

---
+++ arreglo_en_c#_5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{ 
+    static int busquedaLineal(int[] arreglo, int valor)
+    {
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            if (arreglo[i] == valor)
+            {
+                return i; // devuelve la posicioon
+            }
+        }
+
+        return -1; // indica que no se encontro la posicioon
+    }
+
+    static void Main()
+    {
+        int[] numeros = { 4, 8, 15, 16, 23, 42 };
+
+        Console.Write("Ingrese el numero que quiere encontrar en el arreglo: ");
+        int valor = int.Parse(Console.ReadLine());
+
+        int posicion = busquedaLineal(numeros, valor);
+
+        if (posicion != -1)
+        {
+            Console.WriteLine("Numero encontrado en la posicion: " + posicion);
+        }
+        else
+        {
+            Console.WriteLine("El numero no se encuentra en el arreglooo");
+        }
+    }
+}
+
+
