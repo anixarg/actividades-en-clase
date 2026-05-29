d674e4530f57a4dd283f452dfa608c6a340cfed0
anixarg <anabellajk200@gmail.com>
2026-05-29 10:02:19 -0300

    Update EJERCICIO 1.cs

--- EJERCICIO 1.cs
+++ EJERCICIO 1.cs
@@ -4,12 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        // EL USUARIO INGRESA SU EDAD
         Console.Write("Ingrese la edad: ");
         int edad;
         bool esNumero = int.TryParse(Console.ReadLine(), out edad);
 
-        // VALIDACION DEL NUMERO
         if (!esNumero)
         {
             Console.WriteLine("Por favor, ingrese un número válido para la edad.");
@@ -28,7 +26,6 @@ class Program
         }
     }
 
-    // VERIFICACION DE QUE SI ES MAYOR
     public static bool EsMayorDeEdad(int edad)
     {
         return edad >= 18;
