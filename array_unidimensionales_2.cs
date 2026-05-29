ac4e25001e5fc08a9c1a5cd7618bef7ccf9c1b62
anixarg <anabellajk200@gmail.com>
2026-05-29 09:59:18 -0300

    Add files via upload

---
+++ array_unidimensionales_2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static int busquedaL(int[] arreglo, int numero)
+    {
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            if (arreglo[i] == numero)
+            {
+                return i; // devuelve la posicioon
+            }
+        }
+
+        return -1;
+    }
+
+    static void Main()
+    {
+        int[] datos = { 5, 3, 1, 2, 4 };
+
+        Console.Write("Ingrese el numero que quiere encontrar en el arreglo: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int resultado = busquedaL(datos, numero);
+
+        if (resultado != -1)
+        {
+            Console.WriteLine("Numero encontrado en la posicion: " + resultado);
+        }
+        else
+        {
+            Console.WriteLine("El numero no se encuentra en el arreglooo");
+        }
+    }
+}
+
